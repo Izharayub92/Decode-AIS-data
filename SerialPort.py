0a19b683751f291c0f65f27d62684edf2dc17c76
MOHAMAD IZHAR BIN AYUB <70748797+Izharayub92@users.noreply.github.com>
2022-07-21 15:35:22 +0800

    Update SerialPort.py

--- SerialPort.py
+++ SerialPort.py
@@ -4,7 +4,7 @@ import serial
 #Use module pyais sub IterMessages
 from pyais import IterMessages
 
-#Undecoded data discription
+#Undecoded data discription (Open your device manager and check the serial port COM1,COM2 or COM3)
 ser = serial.Serial('COM3', 9600)
 data = ser.readline()
 
